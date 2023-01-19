732dda752763f040f798104f58b17adfcc0c873c
valdooliver <edvaldo.soza@gmail.com>
2023-01-19 14:13:03 -0300

    novo commit

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-function HomePage(){
-  return (
-    <div> Hello world, NextJs + ReactJs 2</div>
-  )
-}
-
-export default HomePage
-
-
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Title from '../src/components/title/title'
+
+function HomePage(){
+  return (
+    <div> 
+      <Title/>
+    Hello world, NextJs + ReactJs 2</div>
+  )
+}
+
+export default HomePage
+
+
