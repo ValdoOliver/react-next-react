d4af46be287a8a756e27ad7ca2944ad664a5bcd0
valdooliver <edvaldo.soza@gmail.com>
2023-01-19 20:29:19 -0300

    UseState

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,21 @@
+import React, { useState } from 'react';
 import Head from 'next/head'
 
 import Title from '../src/components/title/title'
 import SubTitle from '../src/components/SubTitle/SubTitle'
+import Button from '../src/components/button/Button.jsx'
 
 function HomePage(){
+
+  const [clicando,setClick] = useState(0) 
+  /* setclick pode usar qualquer nome
+   useTate só pode ser usado dentro de funcões
+  */
+   const handleClick = ()=> {
+     console.log('HANDLECLICK')
+     setClick(clicando + 1)
+   }
+
   return (
 <>
     <Head>
@@ -16,7 +28,13 @@ function HomePage(){
       <p>Hello world, NextJs + ReactJs 2</p>
       <Title> um titulo</Title>
       <SubTitle text='Outro subtitulo'/>
-      <p>Hello world, NextJs + ReactJs 2</p>
+      <Button onClick={handleClick}
+     
+       >
+        botao teste
+        </Button>
+
+        <div>Clicks {clicando}</div>
     </div>
     </>
   )
